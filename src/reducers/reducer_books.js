7605ca09988d49bd2ae68bd00f6d1e83c5a78269
vertebrand <alvarosg88@gmail.com>
2017-05-15 00:59:10 +0200

    books reducer, book list container, react-redux connect

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title : 'The Silmarillion' },
+        { title : 'The Hobbit' },
+        { title : 'The Fellowship of the Ring' },
+        { title : 'The Two Towers' },
+        { title : 'The Return of the King' }
+    ];
+}
